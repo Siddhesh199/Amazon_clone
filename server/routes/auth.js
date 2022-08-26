75ddfc61ff57beb768a3af4c82825e13794557d9
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-08-26 23:56:46 +0530

    Success in hashing the password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
 
 const authRouter = express.Router();
 
@@ -13,9 +14,11 @@ authRouter.post("/api/signup", async (req, res) => {
       return res.status(400).json({ msg: "User already exists" });
     }
 
+    const hashedPassword = await bcryptjs.hash(password, 8);
+
     let user = new User({
       email,
-      password,
+      password: hashedPassword,
       username,
     });
     user = await user.save();
