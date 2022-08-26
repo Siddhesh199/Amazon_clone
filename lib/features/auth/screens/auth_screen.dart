95d69e80132001d0edbc409680b4def644a4f151
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-08-27 02:28:57 +0530

    User signUp success

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone/common/widgets/custom_button.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/auth/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 import '../../../common/widgets/custom_textfield.dart';
@@ -21,7 +22,7 @@ class _AuthScreenState extends State<AuthScreen> {
   Auth _auth = Auth.signup;
   final _signUpFormKey = GlobalKey<FormState>();
   final _signInFormKey = GlobalKey<FormState>();
-
+  final AuthService authService = AuthService();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
@@ -33,6 +34,15 @@ class _AuthScreenState extends State<AuthScreen> {
     _emailController.dispose();
     _passwordController.dispose();
     _usernameController.dispose();
+  }
+
+  void signUpUser() {
+    authService.signUpUser(
+      context: context,
+      email: _emailController.text,
+      password: _passwordController.text,
+      username: _usernameController.text,
+    );
   }
 
   @override
@@ -96,7 +106,12 @@ class _AuthScreenState extends State<AuthScreen> {
                           hintText: 'Password',
                         ),
                         const SizedBox(height: 10),
-                        CustomButton(text: 'Sign Up', onTap: () {}),
+                        CustomButton(text: 'Sign Up', onTap: () {
+                          if(_signUpFormKey.currentState!.validate()){
+                            signUpUser();
+                            print("Hello");
+                          }
+                        }),
                       ],
                     ),
                   ),
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const DB = 'mongodb+srv://sid:naruto9976@cluster0.tucwjjl.mongodb.net/?retryWrites=true&w=majority';
 
