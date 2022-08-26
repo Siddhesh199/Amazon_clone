fa7db48b326a942ba3939ac873d6c4aa2cb1e495
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-08-27 01:14:36 +0530

    Created showSnackBar utility

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,4 +1,6 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/models/user.dart';
+import 'package:http/http.dart' as http;
 
 class AuthService {
   //sign up user
@@ -16,6 +18,14 @@ class AuthService {
         address: '',
         type: '',
         token: '',
+      );
+
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signup'),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
       );
     } catch (e) {}
   }
