de3c4b170021e01874a5eaa7ca0509854c64a19f
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-08-27 02:51:22 +0530

    Created signIn API in backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const authRouter = express.Router();
 
 authRouter.post("/api/signup", async (req, res) => {
   //get the data from the user
+  //post that data in database
+  //return that data to the user
   try {
     const { username, email, password } = req.body;
 
@@ -23,8 +26,25 @@ authRouter.post("/api/signup", async (req, res) => {
     });
     user = await user.save();
     res.json(user);
-    //post that data in database
-    //return that data to the user
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body; 
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "User not found" });
+    }
+    const isMatch = bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Incorrect password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+    res.json({token, ...user._doc});
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
