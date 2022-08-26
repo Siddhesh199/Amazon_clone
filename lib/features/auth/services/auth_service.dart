e82471ba2dc6c7cbba49d91d50a78760bb9a5828
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-08-27 01:25:38 +0530

    Created signUp user function on client side

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,10 +1,14 @@
+import 'package:amazon_clone/constants/error_handling.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/constants/utils.dart';
 import 'package:amazon_clone/models/user.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
   //sign up user
   void signUpUser({
+    required BuildContext context,
     required String email,
     required String password,
     required String username,
@@ -27,6 +31,17 @@ class AuthService {
           'Content-Type': 'application/json; charset=UTF-8',
         },
       );
-    } catch (e) {}
+      httpErrorHandle(
+          response: res,
+          context: context,
+          onSuccess: () {
+            showSnackBar(
+              context,
+              'Account generated! Sign In with the same credentials',
+            );
+          });
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
   }
 }
