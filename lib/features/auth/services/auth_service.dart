e3eeb13d5b7b7756768dc8f4ffef49381c9462a4
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-08-27 13:44:14 +0530

    Added function to check the existence of a valid token in backend

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -67,7 +67,6 @@ class AuthService {
           'Content-Type': 'application/json; charset=UTF-8',
         },
       );
-      print(res.body);
       httpErrorHandle(
           response: res,
           context: context,
@@ -79,6 +78,44 @@ class AuthService {
             Navigator.pushNamedAndRemoveUntil(
                 context, HomeScreen.routeName, (route) => false);
           });
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  void getUserData(
+    BuildContext context,
+  ) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
+
+      if (token == null) {
+        prefs.setString('x-auth-token', '');
+      }
+
+
+      // http.Response res = await http.post(
+      //   Uri.parse('$uri/api/signin'),
+      //   body: jsonEncode({
+      //     'email': email,
+      //     'password': password,
+      //   }),
+      //   headers: <String, String>{
+      //     'Content-Type': 'application/json; charset=UTF-8',
+      //   },
+      // );
+      // httpErrorHandle(
+      //     response: res,
+      //     context: context,
+      //     onSuccess: () async {
+      //       SharedPreferences prefs = await SharedPreferences.getInstance();
+      //       Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+      //       await prefs.setString(
+      //           'x-auth0-token', jsonDecode(res.body)['token']);
+      //       Navigator.pushNamedAndRemoveUntil(
+      //           context, HomeScreen.routeName, (route) => false);
+      //     });
     } catch (e) {
       showSnackBar(context, e.toString());
     }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -33,7 +33,7 @@ authRouter.post("/api/signup", async (req, res) => {
 
 authRouter.post("/api/signin", async (req, res) => {
   try {
-    const { email, password } = req.body; 
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ msg: "User not found" });
@@ -44,7 +44,22 @@ authRouter.post("/api/signin", async (req, res) => {
     }
 
     const token = jwt.sign({ id: user._id }, "passwordKey");
-    res.json({token, ...user._doc});
+    res.json({ token, ...user._doc });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+authRouter.post("/tokenIsValid", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+    const verified = jwt.verify(token, "passwordKey");
+    if (!verified) return res.json(false);
+
+    const user = await User.findById(verified.id);
+    if (!user) return res.json(false);
+    res.json(true);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
