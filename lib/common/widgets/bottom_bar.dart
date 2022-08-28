79987202e91eb273b14b1b63f577db109f6e9a7d
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-08-28 12:44:25 +0530

    Added functionality to bottom navigation bar

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,5 +1,7 @@
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 
 class BottomBar extends StatefulWidget {
   static const String routeName = '/actual-home';
@@ -16,7 +18,6 @@ class _BottomBarState extends State<BottomBar> {
 
   Icon item1 = const Icon(Icons.home_outlined);
   Icon item2 = const Icon(Icons.person_outline_outlined);
-  Icon item3 = const Icon(Icons.home_outlined);
 
   BottomNavigationBarItem bottomBarItem(Icon icon, int index) {
     return BottomNavigationBarItem(
@@ -36,19 +37,56 @@ class _BottomBarState extends State<BottomBar> {
         label: '');
   }
 
+  List<Widget> pages = [
+    const HomeScreen(),
+    const Center(
+      child: Text('Account Page'),
+    ),
+    const Center(
+      child: Text('Cart Page'),
+    ),
+  ];
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: pages[_page],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _page,
         selectedItemColor: GlobalVariables.selectedNavBarColor,
         unselectedItemColor: GlobalVariables.unselectedNavBarColor,
         backgroundColor: GlobalVariables.backgroundColor,
         iconSize: 28,
+        onTap: updatePage,
         items: [
           bottomBarItem(item1, 0),
           bottomBarItem(item2, 1),
-          bottomBarItem(item3, 2),
+          BottomNavigationBarItem(
+              icon: Container(
+                width: bottomBarWidth,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                        color: _page == 2
+                            ? GlobalVariables.selectedNavBarColor
+                            : GlobalVariables.backgroundColor,
+                        width: bottomBarBorderWidth),
+                  ),
+                ),
+                child: Badge(
+                  elevation: 0,
+                  badgeContent: const Text('2'),
+                  badgeColor: Colors.white,
+                  child: const Icon(Icons.shopping_cart_outlined),
+                ),
+              ),
+              label: ''),
         ],
       ),
     );
