bcdf12b5168e171656dd0bde38d3a67b59112815
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-09-14 17:02:58 +0530

    Created add to cart API

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,23 +5,35 @@ const User = require("../models/user");
 const userRouter = express.Router();
 
 userRouter.post("/api/add-to-cart", auth, async (req, res) => {
-    try {
-      const {id} = req.body;
-      const product = await Product.findById(id); 
-      let user = await User.findById(req.user);
-      
-      if(user.cart.length == 0){
-        user.cart.push({product, quantity: 1});
-      } else {
-        for(let i = 0; i < user.cart.length; i++){
-            if(user.cart[i].product._id.equals(product._id)){
-                
-            }
+  try {
+    const { id } = req.body;
+    const product = await Product.findById(id);
+    let user = await User.findById(req.user);
+
+    if (user.cart.length == 0) {
+      user.cart.push({ product, quantity: 1 });
+    } else {
+      let isProductFound = false;
+      for (let i = 0; i < user.cart.length; i++) {
+        if (user.cart[i].product._id.equals(product._id)) {
+          isProductFound = true;
         }
       }
-    } catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-  });
 
-  module.exports = userRouter;
+      if (isProductFound) {
+        let productFound = user.cart.find((p) =>
+          p.product._id.equals(product._id)
+        );
+        productFound.quantity += 1;
+      } else {
+        user.cart.push({ product, quantity: 1 });
+      }
+    }
+    user = await user.save();
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+module.exports = userRouter;
