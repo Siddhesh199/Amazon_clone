c5fd9620355c1c5d8e575db747fc47d593fea2d2
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-09-16 14:25:50 +0530

    Created order schema

--- server/routes/user.js
+++ server/routes/user.js
@@ -84,11 +84,20 @@ userRouter.delete("/api/remove-from-cart/:id", auth, async (req, res) => {
 
 userRouter.post("/api/save-user-address", auth, async (req, res) => {
   try {
-    const {address} = req.body;
+    const { address } = req.body;
     let user = await user.findById(req.user);
     user.address = address;
     user = await user.save();
     res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+userRouter.post("/api/order", auth, async (req, res) => {
+  try {
+    const { cart, totalPrice, address } = req.body;
+
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
