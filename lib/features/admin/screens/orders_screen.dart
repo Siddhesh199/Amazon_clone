13462fd085ba6299af013137d0c67e0464a73758
Siddhesh199 <siddheshsalaskar188@gmail.com>
2022-09-21 12:04:29 +0530

    Successfully fetching orders in admin orders screen

--- lib/features/admin/screens/orders_screen.dart
+++ lib/features/admin/screens/orders_screen.dart
@@ -1,3 +1,7 @@
+import 'package:amazon_clone/common/widgets/loader.dart';
+import 'package:amazon_clone/features/account/widgets/single_product.dart';
+import 'package:amazon_clone/features/admin/services/admin_services.dart';
+import 'package:amazon_clone/models/order.dart';
 import 'package:flutter/material.dart';
 
 class OrdersScreen extends StatefulWidget {
@@ -8,9 +12,41 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
+  List<Order>? orders;
+  final AdminServices adminServices = AdminServices();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchOrders();
+  }
+
+  void fetchOrders() async {
+    orders = await adminServices.fetchAllOrders(context);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(itemCount: gridDelegate: , itemBuilder: itemBuilder)
-
+    return orders == null
+        ? const Loader()
+        : Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.builder(
+              itemCount: orders!.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                final orderData = orders![index];
+                return SizedBox(
+                  height: 140,
+                  child: SingleProduct(
+                    image: orderData.products[0].images[0],
+                  ),
+                );
+              },
+            ),
+        );
   }
 }
